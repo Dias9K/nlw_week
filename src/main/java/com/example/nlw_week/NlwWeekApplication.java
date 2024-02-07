c04347c12577deb1c36cf2dc521b5ef3716b1ffb
Dias9K <gabrielmdais557@gmail.com>
2024-02-07 00:09:34 -0300

    configuração inicial

---
+++ src/main/java/com/example/nlw_week/NlwWeekApplication.java
@@ -0,0 +1,14 @@
+package com.example.nlw_week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlwWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlwWeekApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
