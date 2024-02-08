dacf7c5b051ba2a4fababd7d6326d166ebd132f6
Dias9K <gabrielmdais557@gmail.com>
2024-02-07 23:58:20 -0300

    1st controller

--- src/main/java/com/example/nlw_week/NlwWeekApplication.java
+++ src/main/java/com/example/nlw_week/NlwWeekApplication.java
@@ -1,10 +1,10 @@
-package com.example.nlw_week;
+package com.example.nlw_week; //"groupId"
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NlwWeekApplication {
+public class NlwWeekApplication { // artifact
 
 	public static void main(String[] args) {
 		SpringApplication.run(NlwWeekApplication.class, args);
--- src/main/java/com/example/nlw_week/PrimeiraController.java
+++ src/main/java/com/example/nlw_week/PrimeiraController.java
@@ -1,15 +1,47 @@
 package com.example.nlw_week;
 
+// todas essas anotações são "componentes", que baseados em seus nomes, fazem o Spring entender o que é cada trecho
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+/*MÉTODOS COMUNS
+ * - get = buscar uma informação dentro do servidor
+ * - put = alterar uma informação dentro do servidor
+ * - post = inserir uma informação dentro do servidor
+ * - delete = deletar uma informação dentro do servidor
+ * - patch = alterar PONTUALMENTE uma informação dentro do servidor
+ */
 
-@RestController
-@RequestMapping("/primeiraController")
+/*TIPOS DE PARÂMETROS
+ * - Body - cria um corpo para envio de parâmetros { }
+ * - Query Params - parâmetros baseados em query, contendo chaves e valores ex: http://localhost:8085/users?nome=Gabriel&idade=23 (opcional) separados por '&'
+ * - Parâmetros de rotas - fazem parte da rota e têm obrigatoriedade de serem informados ex: http://localhost:8085/users/{nome}/{idade}
+ */
+
+@RestController // começo da API Rest e informando ao compilador que toda a classe se torna uma
+                // Controller
+@RequestMapping("/mapping") // informa qual o recurso/path que o navegador deve procurar ao iniciar a
+                            // aplicação
 public class PrimeiraController {
 
-    @GetMapping("/") // entender melhor o que são e pra que servem essas anotações
-    public String retornoPrimeiraController() {
-        return "Criando a minha primeira Controller";
+    @GetMapping("/retornarMapping") // definição do path para fazer uma espécie de ligação entre o RequestMapping e
+                                    // o método por meio do GetMapping
+    public Usuario retornoPrimeiraController() { // criação do método da execução e que gera um retorno ao usuário
+        var usuario = new Usuario("Gabriel", 23); // quando o compilador sabe qual o tipo do retorno, é possível
+                                                  // utilizar o "var" ao invés de ter que especificar o tipo da variável
+                                                  // ou objeto que se está retornando. E além disso, nessa linha está
+                                                  // ocorrendo a instanciação do objeto
+        return usuario; // retorna o objeto como se fosse um .json
     }
+
+    @PostMapping("/primeiroPost") // o navegador só é capaz de acessar recursos via Get e não Post, para isso é preciso do HttPie
+    public String primeiroPost() {
+        return "Meu primeiro post...";
+    }
+
+    // criação do objeto construtor
+    record Usuario(String nome, int idade) {
+    }
+
 }
